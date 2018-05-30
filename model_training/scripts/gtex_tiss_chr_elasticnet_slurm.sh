efb9757d660790c9338f6e47ed4b72081dd2ce61
rbonazzola <rodbonazzola@gmail.com>
2018-05-30 10:25:55 -0500

    Changed paths.

--- model_training/scripts/gtex_tiss_chr_elasticnet_slurm.sh
+++ model_training/scripts/gtex_tiss_chr_elasticnet_slurm.sh
@@ -6,4 +6,11 @@
 #SBATCH --output=logs/%x.o%j.log
 #SBATCH --error=logs/%x.e%j.err
 
+DATA_FOLDER="/mnt/disks/data/prepared_data_for_model_training
+
+# Download files (not necessary when I use the shared folder in the controller)
+# gsutil cp gs://predictdb/gtex_v8/prepared_data/expressions/${tiss}_Analysis.expression.txt ${DATA_FOLDER}/expressions/${tiss}
+# gsutil cp gs://predictdb/gtex_v8/prepared_data/annotations/gtex_v8_eur_shapeit2_phased_maf01_snp_annot.chr${chrom}.txt ${DATA_FOLDER}/annotations/${chrom}
+# gsutil cp gs://predictdb/gtex_v8/prepared_data/genotypes/gtex_v8_eur_shapeit2_phased_maf01.chr${chrom}.txt.gz ${DATA_FOLDER}/genotypes
+
 Rscript gtex_tiss_chrom_training.R ${tiss} ${chrom}
--- model_training/scripts/gtex_tiss_chrom_training.R
+++ model_training/scripts/gtex_tiss_chrom_training.R
@@ -6,11 +6,13 @@ argv <- commandArgs(trailingOnly = TRUE)
 tiss <- argv[1]
 chrom <- argv[2]
 
-snp_annot_file <- "../../prepare_data/genotype/gtex_v7_hapmapceu_snp_annot.chr" %&% chrom %&% ".txt"
-gene_annot_file <- "../../prepare_data/expression/gencode.v19.genes.patched_contigs.parsed.txt"
-genotype_file <- "../../prepare_data/genotype/gtex_v7_eur_imputed_maf_0.01_R2_0.8_chr" %&% chrom %&% ".txt.gz"
-expression_file <- "../../prepare_data/expression/" %&% tiss %&% "_Analysis.expression.txt"
-covariates_file <- "../../prepare_data/covariates/" %&% tiss %&% "_Analysis.combined_covariates.txt"
+shared_data_folder <- "/apps/shared_data/"
+
+snp_annot_file <- shared_data_folder %&% "annotations/" %&% "gtex_v8_eur_shapeit2_phased_maf01_snp_annot.chr" %&% chrom %&% ".txt" 
+gene_annot_file <- shared_data_folder %&% "annotations/" %&% "gencode_v26_parsed.txt" 
+genotype_file <- shared_data_folder %&% "genotypes/" %&% "gtex_v8_eur_shapeit2_phased_maf01.chr" %&% chrom %&% ".txt.gz"
+covariates_file <- shared_data_folder %&% "covariates/" %&% tiss %&% "_Analysis.combined_covariates.txt"
+expression_file <- shared_data_folder %&% "expressions/" %&% tiss %&% "_Analysis.expression.txt"
 prefix <- tiss %&% "_nested_cv"
 
 main(snp_annot_file, gene_annot_file, genotype_file, expression_file, covariates_file, as.numeric(chrom), prefix, null_testing=FALSE)
